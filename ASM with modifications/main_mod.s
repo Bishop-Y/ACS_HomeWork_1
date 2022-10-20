89df173ed8cebb24fc4b617c084b02f5f42287cf
Bishop-Y <66753948+Bishop-Y@users.noreply.github.com>
2022-10-21 00:55:33 +0300

    Update main_mod.s

--- ASM with modifications/main_mod.s
+++ ASM with modifications/main_mod.s
@@ -4,7 +4,7 @@
 	.section	.rodata
 	.align 8
 .LC0:
-	.string	"Enter input length (0 < length <= 9):"
+	.string	"Enter input length (0 < length <= 20):"
 .LC1:
 	.string	"%d"
 .LC2:
@@ -182,7 +182,7 @@ main:
 	test	eax, eax
 	jle	.L10
 	mov	eax, DWORD PTR -116[rbp]
-	cmp	eax, 9
+	cmp	eax, 20
 	jle	.L11
 .L10:
 	# mov eax, DWORD PTR -116[rbp]
